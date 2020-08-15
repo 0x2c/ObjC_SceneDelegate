fb11667fa1783577b3db5146979e6bb4c20ba91e
0x2c <eg.proxy@gmail.com>
2020-08-15 02:27:35 -0700

    OBJCSD1 :: Initial Commit / Multiple Scene Management

--- ObjC_SceneDelegate/AppDelegate.m
+++ ObjC_SceneDelegate/AppDelegate.m
@@ -19,8 +19,25 @@
 
 #pragma mark - UISceneSession lifecycle
 
-
+/* This delegate is called on first launch or user action, meaning:
+* 1) App was installed and launched for the first time.
+* 2) Device had restarted and launched for the first time.
+* 3) User action in creating a new multitasking window.
+*/
 - (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
+    /* UISceneConfiguration Object Creation
+    * initWithName :: Appears as a value for info.plist key "Configuration Name"
+    * sessionRole :: There are two types of roles which relay to the system info about where the scene should be rendered.
+    *                In most cases pass struct's `role` value from delegate.
+                    [!] As it were from the source itself,
+                     * // A session role which defines a typical interactive application on a device's main display
+                     * UIKIT_EXTERN UISceneSessionRole const UIWindowSceneSessionRoleApplication API_AVAILABLE(ios(13.0));
+                     * // A session role which defines an interface for a non-main external display
+                     * UIKIT_EXTERN UISceneSessionRole const UIWindowSceneSessionRoleExternalDisplay API_AVAILABLE(ios(13.0));
+                     Plainly speaking, UIWindowSceneSessionRoleApplication is for your device display and
+                     UIWindowSceneSessionRoleExternalDisplay is for displaying externally like how the app AirPlay mirrors
+                     your device display to another monitor.
+    */
     return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
 }
 
